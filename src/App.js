d6c78acd2e02c8821d79b4b05b85ce541d5b53ef
Anna Grasmück <grasmueck.anna@gmail.com>
2022-06-27 18:02:11 +0200

    Render Database

--- src/App.js
+++ src/App.js
@@ -1,6 +1,40 @@
-export default function App() {
-  return <div>hello
+import { useState } from 'react';
+import styled from 'styled-components';
 
-  </div>;
+import ChairList from './component/ChairList';
+import data from './data.json';
+
+
+export default function App() {
+  const [imageError, setImageError] = useState(false);
+
+
+  return (
+    <AppContainer>
+      <h1>Find your perfect Design Chair</h1>
+      <MainContainer>
+        {data.map(item => (
+          <ChairList key={item._id} name={item.name} mainImg={item.imgUrl} imageError={imageError} setImageError={setImageError} />
+        ))}
+      </MainContainer>
+    </AppContainer>
+  );
 }
 
+const AppContainer = styled.div`
+  display: flex;
+  flex-flow: wrap column;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  background-color: white;
+  
+`;
+
+const MainContainer = styled.div`
+  max-width: 80%;
+  height: min-content;
+  background-color: var(--secondary-color);
+  
+  
+`;
---
+++ src/component/ChairList.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+
+export default function ChairList({name, mainImg}) {
+  console.log(event => event.target.src);
+
+  return (
+    <ChairItem>
+      <ChairHeader>{name}</ChairHeader>
+      <ChairDetails>
+        <img src={mainImg} alt={name} width="200" />
+        <button>Description</button>
+      </ChairDetails>
+    </ChairItem>
+  );
+}
+
+const ChairItem = styled.section`
+  display: flex;
+  flex-flow: column wrap;
+  border: solid 4px black;
+  margin: 2rem;
+`;
+
+const ChairHeader = styled.header`
+  align-self: center;
+  font-size: 2rem;
+  font-weight: 800;
+  margin: 1rem;
+`;
+
+const ChairDetails = styled.article`
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-around;
+  margin-bottom: 1rem;
+`;
