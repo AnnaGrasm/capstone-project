d261351821008a270e002bd0b0f16c37637c8692
Anna Grasmück <grasmueck.anna@gmail.com>
2022-07-05 12:59:17 +0200

    Add underline in Link

--- src/component/DesignerLink/DesignerLink.js
+++ src/component/DesignerLink/DesignerLink.js
@@ -1,22 +1,21 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export default function DesignerLink({designer, designerURL}) {
   return (
-    <>
-      <Link href={designerURL} target="_blank" rel="noreferrer">
-        Designed by <span>{designer}</span>
-      </Link>
-    </>
+    <Link href={designerURL} target="_blank" rel="noreferrer">
+      Designed by <span>{designer}</span>
+    </Link>
   );
 }
 
-const Link = styled.a `
-    text-decoration: none;
-    color: black;
-    
-
-    span{
-      font-weight: 700;
+const Link = styled.a`
+  text-decoration: none ;
+  color: black;
+  
+  span {
+    font-weight: 700;
     font-size: 1rem;
-    }
-`
+    text-decoration: underline;
+  }
+
+`;
